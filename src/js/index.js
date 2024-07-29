c52c1f9e018d2f12c4e4dc2799cb7224d16068d6
Mateus Fernandes <mateusveloso2608@gmail.com>
2024-07-28 23:44:34 -0300

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,9 @@
+function abrirMenu() {
+    document.getElementById('menuNav').style.width = '100%';
+}
+
+function fecharMenu() {
+    document.getElementById('menuNav').style.width = '0';
+    document.getElementsByClassName('.navegar').style.width = '0';
+}
+
